ae25e952ba87bc99c5ef3988ea1e40e9a6064a25
AzhelOleg <azhelo@yandex.ru>
2019-11-13 11:20:57 +0300

    1. Merged with Vadim 2. Added gradient

--- Сделать экран "Задачи" как в Trello/Screens/WelcomeViewController.swift
+++ Сделать экран "Задачи" как в Trello/Screens/WelcomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  WelcomViewController.swift
+//  SplashViewController.swift
 //  AppStructure
 //
 //  Created by XCodeClub on 2019-11-08.
@@ -8,25 +8,59 @@
 
 import UIKit
 
-final class WelcomeViewController: UIViewController {
+class WelcomeViewController: UIViewController {
+    
+    var gameTimer : Timer?
+    
+    let welcomeTextView: UITextView = {
+        let textView = UITextView()
+        textView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
+        let attributedText = NSMutableAttributedString(string: "Добро", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 24)])
+        attributedText.append(NSAttributedString(string: "\nпожаловать!", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 24)]))
+        textView.attributedText = attributedText
+        textView.textAlignment = .center
+        textView.isEditable = false
+        textView.isScrollEnabled = false
+        return textView
+    }()
+    
+    let startButton: GradientButton = {
+        let button = GradientButton(type: .system)
+        button.setTitle("Начать", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.textAlignment = .center
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+        button.frame = CGRect(x: 0, y: 0, width: 200, height: 50)
+        button.layer.cornerRadius = 10
+        button.addTarget(self, action: #selector(startButtonTapped), for: .touchUpInside)
+        button.backgroundColor = .blue
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+		
+		
         view.backgroundColor = .white
-        // title
-        let welcomeTextView = UITextView.system(with: "Добро" + "\n" + "пожаловать!")
+        
         welcomeTextView.center = view.center
         view.addSubview(welcomeTextView)
-        // button
-        let startButton = UIButton.system(with: "Начать")
-        startButton.addTarget(self, action: #selector(startButtonTapped), for: .touchUpInside)
+        
         startButton.center = view.center
         view.addSubview(startButton)
+		animationButton()
+		gameTimer = Timer.scheduledTimer(withTimeInterval: 3, repeats: true, block: {[unowned self] _ in
+			self.animationButton()
+		})
     }
-    
-    
+	
+	override func viewWillDisappear(_ animated: Bool) {
+		super.viewWillDisappear(animated)
+		self.gameTimer?.invalidate()
+	}
+  
     @objc
-    private func startButtonTapped() {
+    func startButtonTapped() {
         if UserDefaults.standard.isLoggedIn() {
             // navigate to main screen
             AppDelegate.shared.rootViewController.switchToMainScreen()
@@ -36,4 +70,21 @@ final class WelcomeViewController: UIViewController {
         }
     }
     
+    @objc
+    private func animationButton(){
+        self.startButton.gradientLayer.startPoint = CGPoint(x: 0.0, y: 1)
+        self.startButton.gradientLayer.endPoint = CGPoint(x: 1, y: 0.0)
+        let colors:[UIColor] = [#colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1),#colorLiteral(red: 0, green: 0.5898008943, blue: 1, alpha: 1),#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)]
+        self.startButton.gradientLayer.colors = colors.map({ (color) in
+            color.cgColor
+        })
+        let locations: [NSNumber] = [1,1,1]
+        self.startButton.gradientLayer.locations = locations
+        let gradientAnimation = CABasicAnimation(keyPath: "locations")
+        gradientAnimation.fromValue = [0.0, 0.0, 0.25]
+        gradientAnimation.toValue = [0.75, 1, 1]
+		gradientAnimation.duration = 2
+        gradientAnimation.repeatCount = 1
+        startButton.gradientLayer.add(gradientAnimation, forKey: nil)
+	}
 }
